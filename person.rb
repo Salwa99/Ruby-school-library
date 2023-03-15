7c1594ec110f439ac70162accebd76a9413d2fce
Salwa99 <s.ritage99@gmail.com>
2023-03-15 10:01:49 +0100

    Fix the TrimmerDecorator class

--- person.rb
+++ person.rb
@@ -6,7 +6,7 @@ class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name, parent_permission = `true`)
+  def initialize(age, name, parent_permission: true)
     super()
     @id = generate_unique_id
     @name = name
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,6 +2,6 @@ require_relative 'name_decorator'
 
 class TrimmerDecorator < NameDecorator
   def correct_name
-    @nameable.correct_name[0..10]
+    @nameable.correct_name[0...10]
   end
 end
